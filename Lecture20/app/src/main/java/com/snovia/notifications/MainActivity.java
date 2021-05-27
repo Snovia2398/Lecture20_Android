e4e82f7cdfa972a31ce9421da23422f6c520de8c
Snovia2398 <snovia2398@gmail.com>
2021-05-27 15:38:38 +0500

    Notification displayed

--- Lecture20/app/src/main/java/com/snovia/notifications/MainActivity.java
+++ Lecture20/app/src/main/java/com/snovia/notifications/MainActivity.java
@@ -20,6 +20,21 @@ public class MainActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void ShowNotification(View view) {
-       
+        int notifyID = 1;
+        String CHANNEL_ID = "my_channel_01";
+        CharSequence name = getString(R.string.channel_name);
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+        Notification notification = new Notification.Builder(MainActivity.this)
+                .setContentTitle("New Message")
+                .setContentText("You've received new messages.")
+                .setSmallIcon(R.drawable.ic_android_black_24dp)
+                .setChannelId(CHANNEL_ID)
+                .build();
+
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(mChannel);
+
+        notificationManager.notify(1, notification);
     }
 }
