2bbe558c7a404b2598e0ab1d277458586956056d
Snovia2398 <snovia2398@gmail.com>
2021-05-27 15:29:40 +0500

    Image added in drawable

--- Lecture20/app/src/main/java/com/snovia/notifications/MainActivity.java
+++ Lecture20/app/src/main/java/com/snovia/notifications/MainActivity.java
@@ -1,7 +1,12 @@
 package com.snovia.notifications;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void ShowNotification(View view) {
+       
     }
 }
